6c66c3908b2514e951b58e61dc9bd7219c8ee139
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-02-28 23:36:47 +0000

    Memory management - complete

--- memory_management/src/main.zig
+++ memory_management/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const print = std.debug.print;
+const testing = std.testing;
 
 // MEMORY MANAGEMENT
 
@@ -13,3 +14,105 @@ const print = std.debug.print;
 // tied to function lifetimes like the stack is
 
 pub fn main() !void {}
+
+// take an output variable, return number of bytes written into it
+fn concatLen(a: []const u8, b: []const u8, out: []u8) usize {
+    // ensure we have enough memory
+    std.debug.assert(out.len >= (a.len + b.len));
+
+    // copy the bytes
+    // NOTE: std.mem.copy is deprecated!
+    // @memcpy requires destination and source to be equal length
+    std.mem.copy(u8, out, a);
+    std.mem.copy(u8, out[a.len..], b);
+
+    // return the number of bytes copied
+    return a.len + b.len;
+}
+
+test "concatLen" {
+    const h: []const u8 = "Howdy, ";
+    const w: []const u8 = "world!";
+
+    // output buffer
+    var buf: [128]u8 = undefined; // memory is set aside but not initialised
+
+    // write to buf and receive length
+    const len = concatLen(h, w, &buf);
+
+    // test string equality
+    try testing.expectEqualStrings(h ++ w, buf[0..len]);
+
+    // we could also have combined both the above into
+    try testing.expectEqualStrings(h ++ w, buf[0..concatLen(h, w, &buf)]);
+}
+
+// take an output variable, return a slice from it
+fn concatSlice(a: []const u8, b: []const u8, out: []u8) []u8 {
+    // ensure we have enough memory
+    std.debug.assert(out.len >= (a.len + b.len));
+
+    // copy the bytes
+    // NOTE: std.mem.copy is deprecated!
+    // @memcpy requires destination and source to be equal length
+    std.mem.copy(u8, out, a);
+    std.mem.copy(u8, out[a.len..], b);
+
+    // return the number of bytes copied
+    return out[0 .. a.len + b.len];
+}
+
+test "concatSlice" {
+    const h: []const u8 = "Howdy, ";
+    const w: []const u8 = "world!";
+
+    // output buffer
+    var buf: [128]u8 = undefined; // memory is set aside but not initialised
+
+    // write to buf and receive length
+    const slice = concatSlice(h, w, &buf);
+
+    // test string equality
+    try testing.expectEqualStrings(h ++ w, slice);
+}
+
+// take an allocator, return bytes allocated with it, called must free bytes
+fn concatAlloc(allocator: std.mem.Allocator, a: []const u8, b: []const u8) ![]u8 {
+    // allocate required space
+    const bytes = try allocator.alloc(u8, a.len + b.len);
+    //errdefer allocator.free(bytes); // free the memory ONLY in the event of an error
+
+    // copy the bytes
+    // NOTE: std.mem.copy is deprecated!
+    // @memcpy requires destination and source to be equal length
+    std.mem.copy(u8, bytes, a);
+    //try mayFail(); // <- will cause a memory leak without `errdefer`!
+    std.mem.copy(u8, bytes[a.len..], b);
+
+    // return the bytes
+    return bytes;
+}
+
+test "concatAlloc" {
+    const h: []const u8 = "Howdy, ";
+    const w: []const u8 = "world!";
+    const allocator = std.testing.allocator; // testing allocator will detect memory leaks
+
+    // write to buf and receive slice
+    const slice = try concatAlloc(allocator, h, w);
+    defer allocator.free(slice); // defer a `free()` to prevent memory leak!
+
+    // `defer` in Zig executes when exiting scope, not at when the function returns!
+
+    // test string equality
+    try testing.expectEqualStrings(h ++ w, slice);
+}
+
+fn mayFail() !void {
+    return error.Boom;
+}
+
+// test foo in sections.zig
+test {
+    _ = @import("sections.zig");
+}
--- memory_management/src/sections.zig
+++ memory_management/src/sections.zig
@@ -3,8 +3,105 @@ const std = @import("std");
 // memory sections or where are the bytes?
 
 // these are stored in the global constant section of memory
-const pi: f64 = 3.141592;
+// these ar comptime know values
+const pi: f64 = 3.141592653589793;
 const greeting = "Howdy";
 
 // this is stored in the global data section of memory
+// the size of this variable is known at comptime, so even though it is a
+// variable the required amount of memory can be set aside
 var count: usize = 0;
+
+fn locals() u8 {
+    // all of these local variable are gone once the nfunction returns or
+    // exits, they are on the STACK FRAME
+    var a: u8 = 1;
+    var b: u8 = 2;
+    var result: u8 = a + b;
+
+    // since it is a primitive type, result is returned as a copy
+    return result;
+}
+
+fn invalidPtr() *u8 {
+    // `x` is on the stack
+    var x: u8 = 100;
+    // we return a pointer but because x` is gone once the function returns
+    // we are returning an invalid pointer
+    return &x;
+}
+
+fn anotherInvalidPtr() []u8 {
+    var array: [5]u8 = .{ 'H', 'e', 'l', 'l', 'o' };
+
+    // NOTE: a slice is a pointer!
+    var s = array[1..];
+
+    // returning `s` will cause an error as we are rturning a DANGLING POINTER
+    // after we return s, `array` will be gone and our slice will be pointing
+    // at nothing
+    return s;
+}
+
+// the way we solve the problem of data being removed when it leaves the stack
+// is to place what we need on the HEAP
+
+fn validPtr(allocator: std.mem.Allocator) allocator.Error![]u8 {
+    // we pass in an allocator as a parameter, and we return either an
+    // allocator error or a u8 slice (which is a pointer)
+
+    var array: [5]u8 = .{ 'H', 'e', 'l', 'l', 'o' }; // sam array as above
+
+    const s = try allocator.alloc(u8, 5); // allocate memory on the heap
+
+    //std.mem.copy(u8, s, array); // std.mem.copy deprecated!
+    @memcpy(s, &array); // copy the array to the heap
+
+    // return a pointer to our copy of the array in the heap, the copy on the
+    // heap will outlive anything in the stackframe and will persist until we
+    // specifically free that memory, the caller is responsible for freeing it
+    return s;
+}
+
+const Foo = struct {
+    s: []u8,
+
+    // when a type needs to initialise resources such as allocating memory
+    // it is typically done using an `init()` function
+
+    // return error union with pointer to a Foo
+    fn init(allocator: std.mem.Allocator, s: []const u8) !*Foo {
+        // we use `create()` ro allocate a single item on the heap
+        // which returns a pointer
+        const foo_ptr = try allocator.create(Foo);
+
+        // errdefer a `destroy()` to free the memory in the event of an error
+        errdefer allocator.destroy(foo_ptr);
+
+        // `alloc()` allocates memory on the heap and return a slice
+        foo_ptr.s = try allocator.alloc(u8, s.len);
+
+        // copy s to foo_ptr.s
+        @memcpy(foo_ptr.s, s);
+
+        return foo_ptr;
+    }
+
+    // when a type needs to clean up created/allocated resources this is
+    // done with a `deinit()` method
+    fn deinit(self: *Foo, allocator: std.mem.Allocator) void {
+        // `free()` the allocated memory
+        allocator.free(self.s);
+
+        // `destroy()` our pointer
+        allocator.destroy(self);
+    }
+};
+
+test "Foo" {
+    const allocator = std.testing.allocator;
+    var foo = try Foo.init(allocator, greeting);
+    defer foo.deinit(allocator);
+
+    try std.testing.expectEqualStrings(greeting, foo.s);
+}
